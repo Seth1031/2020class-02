83acd9cdb080efb23e8cee805d1a6ef2034e58e6
Seth <skeror@gmail.com>
2020-03-09 16:42:29 +0800

    Initial Commit

---
+++ 2020class-02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020class-02
+//
+//  Created by Seth on 2020/3/9.
+//  Copyright © 2020 Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
